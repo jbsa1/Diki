377b409fa0cb6595e638d8b2ddeda9834a210dad
yayan2130 <yayan2130@gmail.com>
2021-08-09 10:45:43 +0700

    a

--- default/reset.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-unset($_SESSION["nama"]);
-unset($_SESSION["umur"]);
-unset($_SESSION["jawaban"]);
-unset($_SESSION["hasil_analisis"]);
-
-header('location:datahasil.php');
